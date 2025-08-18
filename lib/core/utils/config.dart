182e359072ff2b340f5f0b33502f388db4c7f997
Abdulazeez5897 <137459978+Abdulazeez5897@users.noreply.github.com>
2025-08-18 06:58:58 +0100

    new update for codebase.

---
+++ lib/core/utils/config.dart
@@ -0,0 +1,27 @@
+/// @author Usman Abdulazeez
+/// email: abdulazeezusman732@gmail.com
+/// Aug, 2025
+///
+
+class AppConfig {
+  // static const String baseUrl = "https://api.afriprize.com/api/v1/";
+  // static const String baseUrl = "https://staging-server.afriprize.com/api/v1/";
+
+  static const String baseUrl = "https://247remotejobs.com/api/";
+
+  // static const String baseUrl = "http://192.168.0.179:3001/api/v1/";
+
+
+  static const String flutterWaveBaseUrl = 'https://api.flutterwave.com/v3';
+  static const String paystackApiKey = 'pk_live_540bdf095640f7765b0a822f08161087b68df565';
+  static const String paystackApiKeyTest = 'pk_test_aae035e8425d5319b6500ef86c0472474a38a5fd';
+  static const String PAYSTACK_SECRET_KEY = 'sk_live_f3e8cad64fa1b51f9b5e9d857032f6e72bbc1d10';
+  static const String flutterWaveTestKey = 'FLWPUBK_TEST-07c9ee8a719c8e4bf7216e273e7de20f-X';
+  static const String flutterWaveLiveKey = 'FLWPUBK-5c5e8a37a04632f42f61c4e1e872d073-X';
+  static const String youtubeOfficial = 'https://www.youtube.com/@Afriprize/featured';
+  static const String instagramOfficial = 'https://www.instagram.com/afriprize/?hl=en';
+  static const bool isTestMode = false;
+  static const String APPLESTOREURL = 'https://apps.apple.com/ng/app/afriprize-mobile/id6474942596';
+  static const String GOOGLESTOREURL = 'https://play.google.com/store/apps/details?id=com.afriprize.afriprize&pcampaignid=web_share';
+
+}
